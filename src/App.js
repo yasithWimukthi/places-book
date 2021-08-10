354f052c2a0371862a58cad956b168f952acfb97
yasithWimukthi <it19966922@my.sliit.lk>
2021-08-10 23:25:21 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {BrowserRouter,Route,Redirect,Switch} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/components/NewPlace";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact>
+                <Users/>
+            </Route>
+            <Route path="/places/new" exact>
+                <NewPlace/>
+            </Route>
+            <Redirect to="/"/>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/user/pages/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Users(props) {
+    return (
+        <div>
+            users
+        </div>
+    );
+}
+
+export default Users;
