2b26a97814546583bcef4db810e087c8369d4218
yasithWimukthi <it19966922@my.sliit.lk>
2021-08-11 22:13:59 +0530

    create components

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './UsersList.css';
+import UserItem from "./UserItem";
 
-function UsersList(props) {
-    return (
-        <div></div>
-    );
+function UsersList({items}) {
+    if(items.length===0){
+        return(
+            <div className="center">
+                <h2>No users found</h2>
+            </div>
+        )
+    }
+    return(
+        <ul>
+            {
+                items.map(user => {
+                    <UserItem
+                        key={user.id}
+                        id={user.id}
+                        image={user.image}
+                        name={user.name}
+                        placeCount={user.places}
+                    />
+                })
+            }
+        </ul>
+    )
 }
 
 export default UsersList;
