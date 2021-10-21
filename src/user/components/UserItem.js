9525343121d7a86819a3b8bf772bf8fb582d8100
yasithWimukthi <it19966922@my.sliit.lk>
2021-10-21 22:37:45 +0530

    implement card component

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,22 +2,22 @@ import React from 'react';
 import './UserItem.css';
 import Avatar from "../../shared/components/UIElements/Avatar";
 import {Link} from 'react-router-dom';
+import Card from "../../shared/components/UIElements/Card";
 
-function UserItem({image,name,placeCount,id}) {
+function UserItem({image, name, placeCount, id}) {
     return (
         <li className="user-item">
-            <div className="user-item__content">
+            <Card className="user-item__content">
                 <Link to={`/${id}/places`}>
                     <div className="user-item__image">
                         <Avatar image={image} alt={name}/>
-                        {/*<img src={image} alt={name}/>*/}
                     </div>
                     <div className="user-item__info">
                         <h2>{name}</h2>
-                        <h3>{placeCount} {placeCount === 1 ? "Place" : "Places" }</h3>
+                        <h3>{placeCount} {placeCount === 1 ? "Place" : "Places"}</h3>
                     </div>
                 </Link>
-            </div>
+            </Card>
         </li>
     );
 }
