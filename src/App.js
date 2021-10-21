6ce3053010338e79428c0ea019c1c28b82425a11
yasithWimukthi <it19966922@my.sliit.lk>
2021-10-21 23:14:56 +0530

    implement navigation components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import React from "react";
 import {BrowserRouter,Route,Redirect,Switch} from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/components/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" exact>
-                <Users/>
-            </Route>
-            <Route path="/places/new" exact>
-                <NewPlace/>
-            </Route>
-            <Redirect to="/"/>
-        </Switch>
+        <MainNavigation/>
+        <main>
+            <Switch>
+                <Route path="/" exact>
+                    <Users/>
+                </Route>
+                <Route path="/places/new" exact>
+                    <NewPlace/>
+                </Route>
+                <Redirect to="/"/>
+            </Switch>
+        </main>
     </BrowserRouter>
   );
 }
