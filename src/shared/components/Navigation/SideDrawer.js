7979b9f9203a246a92471a0e0b2ef6cf254f25d9
yasithWimukthi <it19966922@my.sliit.lk>
2021-11-02 10:37:07 +0530

    implement side drawer

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {CSSTransition} from 'react-transition-group';
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    const drawer =  <aside className="side-drawer">{props.children}</aside>
-    return ReactDOM.createPortal(drawer,document.getElementById('drawer-hook'))
+    const drawer =  (
+        <CSSTransition
+            in={props.show}
+            timeout={200}
+            classNames="slide-in-left"
+            mountOnEnter
+            unmountOnExit
+        >
+            <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>
+        </CSSTransition>
+    );
+    return ReactDOM.createPortal(drawer,document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
