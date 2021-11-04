b900e64e766ff3a2142eb131dbb4ffdbfe0103d8
yasithWimukthi <it19966922@my.sliit.lk>
2021-11-04 09:40:05 +0530

    implement validations

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './NewPlace.css';
+import Input from "../../shared/components/FormElements/Input";
 
 function NewPlace(props) {
     return (
-        <div>
-            new place
-        </div>
+        <form className="place-form">
+            <Input
+                element="input"
+                type="text"
+                label="Title"
+                validators={[]}
+                errorText="Please input a valid title."
+            />
+        </form>
     );
 }
 
