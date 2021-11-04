c2bda806ebdf09fa6b9a54968b3df739ca7c0437
yasithWimukthi <it19966922@my.sliit.lk>
2021-11-04 11:54:55 +0530

    implement validations

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -56,8 +56,13 @@ const NewPlace = () => {
     });
   }, []);
 
+  const placeSubmitHandler = e =>{
+    e.preventDefault();
+    console.log(formState.inputs);
+  }
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -74,6 +79,15 @@ const NewPlace = () => {
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
         onInput={inputHandler}
+      />
+      <Input
+          id="address"
+          element="input"
+          type="text"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
         ADD PLACE
