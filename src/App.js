d997b288a36e18f7618dc1edd7d96d92d56495fb
yasithWimukthi <it19966922@my.sliit.lk>
2021-11-06 09:42:53 +0530

    implement auth context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from "react";
+import React,{useState,useCallback} from "react";
 import {BrowserRouter,Route,Redirect,Switch} from "react-router-dom";
 import Users from "./user/pages/Users";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
@@ -7,33 +7,47 @@ import UserPlaces from "./places/pages/UserPlaces";
 import NewPlace from "./places/pages/NewPlace";
 import UpdatePlace from "./places/pages/UpdatePlace";
 import Auth from "./user/pages/Auth";
+import {AuthContext} from "./shared/context/auth-context";
 
 function App() {
-  return (
-    <BrowserRouter>
-        <MainNavigation/>
-        <main>
-            <Switch>
-                <Route path="/" exact>
-                    <Users/>
-                </Route>
-                <Route path="/:userId/places" exact>
-                    <UserPlaces/>
-                </Route>
-                <Route path="/places/new" exact>
-                    <NewPlace/>
-                </Route>
-                <Route path="/places/:placeId" exact>
-                    <UpdatePlace/>
-                </Route>
-                <Route path="/auth" exact>
-                    <Auth/>
-                </Route>
-                <Redirect to="/"/>
-            </Switch>
-        </main>
-    </BrowserRouter>
-  );
+
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+    const login = useCallback(() => {
+        setIsLoggedIn(true);
+    },[])
+
+    const logout = useCallback(() => {
+        setIsLoggedIn(false);
+    },[])
+
+    return (
+        <AuthContext.Provider value={{isLoggedIn,login,logout}}>
+            <BrowserRouter>
+                <MainNavigation/>
+                <main>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Users/>
+                        </Route>
+                        <Route path="/:userId/places" exact>
+                            <UserPlaces/>
+                        </Route>
+                        <Route path="/places/new" exact>
+                            <NewPlace/>
+                        </Route>
+                        <Route path="/places/:placeId" exact>
+                            <UpdatePlace/>
+                        </Route>
+                        <Route path="/auth" exact>
+                            <Auth/>
+                        </Route>
+                        <Redirect to="/"/>
+                    </Switch>
+                </main>
+            </BrowserRouter>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
